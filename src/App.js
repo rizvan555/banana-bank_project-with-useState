27adc40f3694c1dec02ed163c9e59c0fe9538371
RizvanKarimov <rizvan@MacBook-Air-von-SuperCoder.fritz.box>
2023-04-06 13:58:10 +0200

    banana-bank_project-with-useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [saldo, setSaldo] = useState("");
+  const [inputValue, setInputValue] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img
+          src="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
+          alt="banana_img"
+          className="logoImg"
+        />
+        <h1 id="title">Banana Bank</h1>
       </header>
+      <main className="konto">
+        <h1>Dein Girokonto</h1>
+        <section className="saldo">{saldo}€</section>
+        <input
+          type="number"
+          className="geldbetrag"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          placeholder="Gib einen Geldbetrag ein"
+        />
+        <div className="button-conatiner">
+          <button
+            onClick={() => {
+              setSaldo((prevSaldo) => {
+                return prevSaldo + inputValue;
+              });
+              setInputValue("");
+            }}
+            className="einzahlen"
+          >
+            Einzahlen
+          </button>
+          <button
+            onClick={() => {
+              setSaldo(() => {
+                return saldo - inputValue;
+              });
+              setInputValue("");
+            }}
+            className="auszahlen"
+          >
+            Auszahlen
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
